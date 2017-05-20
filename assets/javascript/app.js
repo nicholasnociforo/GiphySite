6ad34c934d18a5b11f314d5d2f6a458177bcebf0
Nicholas Nociforo <Nicholas Nociforo>
2017-05-19 19:46:06 -0700

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -43,7 +43,7 @@ $('body').on('click', 'button', function() {
 // $("button").on("click", function() {
  
 var giphyTopic = $(this).attr("id");
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         giphyTopic + "&api_key=dc6zaTOxFJmzC&limit=10";
 
 
